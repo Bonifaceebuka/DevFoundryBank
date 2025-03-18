9b4947cf1365208aaba49cdbb32367c4c2160e64
Boniface <boniface.ebuka@gmail.com>
2025-03-18 14:26:56 +0100

    Modified the middleware to include the decode user details to the req object

--- src/api/middlewares/AppMiddleware.ts
+++ src/api/middlewares/AppMiddleware.ts
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { env } from "../../env";
-import { Logger } from "../../lib/logger";
-
-export function expressAuthentication(req: Request, securityName: string, scopes?: string[]): Promise<any> {
+export function expressAuthentication(req: any, securityName: string, scopes?: string[]): Promise<any> {
     return new Promise((resolve, reject) => {
         if (!req.headers.authorization) {
             return reject(new Error("Unauthorized access!"));
@@ -11,14 +9,14 @@ export function expressAuthentication(req: Request, securityName: string, scopes
 
         const token = req.headers.authorization.split(" ")[1];
 
-        jwt.verify(token, env.jwtConfig.secret, (error, decoded: any) => {
+        jwt.verify(token, env.jwtConfig.secret, (error:any, decoded: any) => {
             if (error || !decoded || !decoded.jwtData) {
                 return reject(new Error("Invalid token!"));
             }
 
             // Attach user data to request
-            // req.authEmail = decoded.jwtData.email;
-            // req.authId = decoded.jwtData.user_id;
+            req.authEmail = decoded.jwtData.email;
+            req.authId = decoded.jwtData.user_id;
 
             resolve(decoded.jwtData);
         });
