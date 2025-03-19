69277f874a0b0472383ed7375a21b1aa3b937df2
Boniface <boniface.ebuka@gmail.com>
2025-03-19 04:51:37 +0100

    DB error fix

--- user-service/src/config/postgres.ts
+++ user-service/src/config/postgres.ts
@@ -19,6 +19,11 @@ export const dataSource = new DataSource({
 });
 
 export const postgresLoader = async () => {
+    console.log("pg.host", pg.host)
+    console.log("pg.port", pg.port)
+    console.log("pg.user", pg.user)
+    console.log("pg.pass", pg.pass)
+    console.log("pg.database", pg.database)
     await dataSource.initialize()
         .then(() => console.log("✅ Connected to PostgreSQL database"))
         .catch((err) => console.error(`❌ Database connection error: ${err}`));
