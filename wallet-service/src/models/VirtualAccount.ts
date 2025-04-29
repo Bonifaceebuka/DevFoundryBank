da98ac7f188bc88820c2eb999252c04a94693bd4
Boniface <boniface.ebuka@gmail.com>
2025-04-29 07:18:57 +0100

    Added Virtual account model to the wallet service

--- wallet-service/src/models/VirtualAccount.ts
+++ wallet-service/src/models/VirtualAccount.ts
@@ -2,7 +2,7 @@ import { Column, Entity } from "typeorm";
 import { BaseModel } from "./base";
 
 @Entity({ name: "virtual_accounts" })
-export default class Wallet extends BaseModel {
+export default class BankAccount extends BaseModel {
     @Column()
     user_id!: string;
 
