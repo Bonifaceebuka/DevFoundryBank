10069e48358ff0b1dedbd0cde9f92e25314a195d
Boniface <boniface.ebuka@gmail.com>
2025-04-29 07:15:47 +0100

    Added Transaction service to the wallet service

--- wallet-service/src/services/VtWalletService.ts
+++ wallet-service/src/services/VtWalletService.ts
@@ -2,18 +2,23 @@ import { Inject, Service } from "typedi";
 import { Logger } from "../common/configs/logger";
 import { CONFIGS } from "../common/configs";
 import axios from "axios";
-import { WalletStatus } from '../models/Wallet';
+import Wallet, { WalletStatus } from '../models/Wallet';
 import { VtWalletRepository } from '../repositories/VtWalletRepository';
 import { AppError } from "../common/errors/AppError";
-import { MESSAGES } from "../common/constants/messages";
+import { dynamic_messages, MESSAGES } from "../common/constants/messages";
 import { ACTIVITY_TYPES } from "../common/constants/activity_types";
-import BankAccountService from "./BanAccountService";
+import BankAccountService from "./BankAccountService";
+import TransactionService from "./TransactionService";
+import WalletTransaction from "../models/Transaction";
+import { TransactionKind, TransactionStatus, TransactionType } from "../common/enums";
+import { refGenerator } from "../common/helpers/utils";
 
 @Service()
 export default class VtWalletService {
     constructor(
         @Inject(()=> Logger) private readonly logger: Logger,
         private readonly bankAccountService: BankAccountService,
+        // private readonly transactionService: TransactionService,
 
     ){
         this.logger = new Logger(VtWalletService.name);
@@ -80,4 +85,63 @@ export default class VtWalletService {
         }
     }
 
+    public async findOneWallet(id:string): Promise<Wallet>{
+        let message = null
+        const conditions = {
+            id
+        }
+        console.log({id})
+        const wallet = await VtWalletRepository.findOneByConditions(conditions);
+        if (!wallet) {
+            message = dynamic_messages.NOT_FOUND(`User wallet`)
+            this.logger.info({
+                activity_type: ACTIVITY_TYPES.USER_BANK_ACCOUNT.CREATION,
+                message,
+                metadata: {
+                    wallet: null
+                }
+            });
+            throw new AppError(message, 404)
+        }
+        else{
+            return wallet;
+        }
+       
+    }
+
+    public async creditOneWallet(id:number, amount: number, user_id: string){
+        const {opening_balance, new_balance} = await VtWalletRepository.fundOne(id, amount);
+        const reference = refGenerator(user_id)
+        const newTransaction={
+            reference,
+            name: 'Fund transfer',
+            user_id,
+            kind: TransactionKind.CREDIT,
+            status: TransactionStatus.SUCCESSFUL,
+            type: TransactionType.TRANSFER,
+            opening_balance: opening_balance.toString(),
+            new_balance: new_balance.toString(),
+        }
+        // const transaction = await this.transactionService.create(newTransaction)
+        // return transaction;
+    }
+
+    public async debitOneWallet(id:number, amount: number, user_id: string)
+    // : Promise<WalletTransaction>
+    {
+        const {opening_balance, new_balance} = await VtWalletRepository.debitOne(id, amount);
+        const reference = refGenerator(user_id)
+        const newTransaction={
+            reference,
+            name: 'Wallet debit',
+            user_id,
+            kind: TransactionKind.CREDIT,
+            status: TransactionStatus.SUCCESSFUL,
+            type: TransactionType.TRANSFER,
+            opening_balance: opening_balance.toString(),
+            new_balance: new_balance.toString(),
+        }
+        // const transaction = await this.transactionService.create(newTransaction)
+        // return transaction;
+    }
 }
