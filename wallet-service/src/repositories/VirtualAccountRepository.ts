5f863efc6bf12ee8a6f2045dd9c841e062ddee49
Boniface <boniface.ebuka@gmail.com>
2025-04-29 07:17:17 +0100

    Added Virtual wallet repository to the wallet service

--- wallet-service/src/repositories/VirtualAccountRepository.ts
+++ wallet-service/src/repositories/VirtualAccountRepository.ts
@@ -1,4 +1,5 @@
 import { dataSource } from "../common/configs/postgres";
+import BankAccount from "../models/VirtualAccount";
 import VirtualAccount from "../models/VirtualAccount";
 
 export const VirtualAccountRepository = dataSource.getRepository(VirtualAccount).extend({
@@ -10,8 +11,16 @@ export const VirtualAccountRepository = dataSource.getRepository(VirtualAccount)
         }
         return this.save(account);
     },
+
     async findByUserId(user_id: string): Promise<VirtualAccount[]> 
     {
         return this.find({where:{user_id}});
+    },
+
+    async findOneByConditions(conditions:object)
+    : Promise<BankAccount|null> 
+    {
+        console.log("hhhhhhhhhhhhh")
+        return this.findOne({where: conditions});
     },
 });
