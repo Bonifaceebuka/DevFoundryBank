44d704d32b13c96d2d5fb76be5dfb0634ed68775
Boniface <boniface.ebuka@gmail.com>
2025-04-29 07:12:26 +0100

    Modified the wallet service's enums

---
+++ wallet-service/src/common/enums/index.ts
@@ -0,0 +1,20 @@
+export enum Currency {
+    NGN = "NGN"
+}
+
+export enum TransactionType {
+    TRANSFER = "TRANSFER",
+    WITHDRWAL = "WITHDRWAL",
+    DEPOSIT = "DEPOSIT",
+}
+
+export enum TransactionKind {
+    CREDIT = "CREDIT",
+    DEBIT = "DEBIT",
+}
+
+export enum TransactionStatus {
+    PENDING = "PENDING",
+    SUCCESSFUL = "SUCCESSFUL",
+    FAILED = "FAILED",
+}
